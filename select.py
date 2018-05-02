f5ef5ef5660da5891e0f5dd168ed04dbb0c161ed
Jake <jakemacnaughton@gmail.com>
2018-05-02 12:49:56 -0400

    took out solutions

--- select.py
+++ select.py
@@ -1,37 +1,37 @@
 # Code your solutions below:
 
 def total_seasons():
-    return "SELECT COUNT(year) FROM babe_ruth_stats;"
+    return ""
 
 def total_seasons_with_ny():
-    return "SELECT COUNT(year) FROM babe_ruth_stats WHERE team = 'NY';"
+    return ""
 
 def most_hr():
-    return "SELECT MAX(HR) FROM babe_ruth_stats;"
+    return ""
 
 def least_hr():
-    return "SELECT MIN(HR) FROM babe_ruth_stats;"
+    return ""
 
 def total_hr():
-    return "SELECT SUM(HR) FROM babe_ruth_stats;"
+    return ""
 
 def average_hr_per_year():
-    return "SELECT AVG(HR) FROM babe_ruth_stats;"
+    return ""
 
 def year_and_games_with_least_hr():
-    return "SELECT year, games FROM babe_ruth_stats WHERE HR = 0;"
+    return ""
 
 def select_yr_and_min_hr_with_at_least_100_games():
-    return "SELECT year, MIN(HR) FROM babe_ruth_stats WHERE games > 100;"
+    return ""
 
 def avg_batting_avg_aliased_as_career_average():
-    return "SELECT AVG(AVG) AS career_average FROM babe_ruth_stats;"
+    return ""
 
 def total_years_and_hits_per_team():
-    return "SELECT team, COUNT(year), SUM(hits) FROM babe_ruth_stats GROUP BY team;"
+    return ""
 
 def total_years_and_hr_per_team_ordered_by_hr():
-    return "SELECT team, COUNT(year), SUM(HR) FROM babe_ruth_stats GROUP BY team ORDER BY HR DESC"
+    return ""
 
 def years_with_on_base_over_300():
-    return "SELECT year, hits + BB AS on_base FROM babe_ruth_stats GROUP BY year HAVING on_base > 300;"
+    return ""
